bfa48f40de9d7404aa7facf435fed425600cabf0
Dmytro <admiral1648@gmail.com>
2023-08-30 13:17:54 +0300

    slick slider

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$ (document).ready(function(){
+    $('.eventsSlider').slick({
+        speed: 800,
+        slidesToShow:3,
+        slidesToScroll: 3,
+        arrows: false,
+        vertical: true,
+        verticalSwiping: true
+    });
+
+    $('.slider').slick({
+        speed: 300,
+        slidesToShow:4,
+        arrows: false
+    });
+    $(".prev-btn").click(function () {
+        $(".slider").slick("slickPrev");
+    });
+    $(".next-btn").click(function () {
+        $(".slider").slick("slickNext");
+    });
+
+    $ (document).ready(function(){
+        $('.sliderArticle').slick({
+            dots: true,
+            speed: 300,
+            arrows: false,
+            slidesToShow: 1
+        });
+        $(".prev-btnArt").click(function () {
+            $(".sliderArticle").slick("slickPrev");
+        });
+        $(".next-btnArt").click(function () {
+            $(".sliderArticle").slick("slickNext");
+        });
+    });
+});
